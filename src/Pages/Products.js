7f1cedae0a9bc80acb79d78d28f9de2068a4023e
Madatha Ganesh <84974493+MadathaGanesh@users.noreply.github.com>
2024-08-06 12:57:19 +0530

    Update Products.js

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -3,7 +3,7 @@ import SingleProductPage from "../Components/SingleProductPage";
 import Data from ".././Data.json";
 
 const Products = () => {
-  const [products, setProducts] = useState(Data.products); // After importing the Data ,we will use here
+  const [products] = useState(Data.products); // After importing the Data ,we will use here
   return (
     <div className="grid grid-cols-3 m-4 p-20">
       {products.map((item) => (
